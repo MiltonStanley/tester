16a4715fd0746175b3cb795223da020f48dc0706
MiltonStanley <milton.stanley@gmail.com>
2012-10-26 15:56:57 -0500

    Added position; need to edit views

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121026184658) do
+ActiveRecord::Schema.define(:version => 20121026205049) do
 
   create_table "employees", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "position"
   end
 
 end
